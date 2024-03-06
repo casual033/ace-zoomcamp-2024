01e5ee84eab364955a0732fafa0e7bb0990469b0
Aleksa Antelj <aleksa.antelj@quinyx.com>
2024-03-06 22:56:32 +0100

    export to postgres and gcs parquet

---
+++ ace-zoomcamp/data_exporters/green_taxi_data_to_big_query.py
@@ -0,0 +1,29 @@
+import pyarrow as pa
+import pyarrow.parquet as pq
+import os
+
+if 'data_exporter' not in globals():
+    from mage_ai.data_preparation.decorators import data_exporter
+
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = "/home/src/first-tine-416310-c0d9815f1ba4.json"
+
+bucket_name = 'mage-zoomcamp-2024-ace'
+object_key = 'green_taxi_data.parquet'
+
+table_name = "green_taxi_data"
+
+root_path = f'{bucket_name}/{table_name}'
+
+@data_exporter
+def export_data(data, *args, **kwargs):
+
+    table = pa.Table.from_pandas(data)
+
+    gcs = pa.fs.GcsFileSystem()
+
+    pq.write_to_dataset(
+        table,
+        root_path = root_path,
+        partition_cols = ['lpep_pickup_date'],
+        filesystem=gcs
+    )
